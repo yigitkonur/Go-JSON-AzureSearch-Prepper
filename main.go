77a769cf4df9c1d6b572a25289903a10ceeed479
Yigit Konur <9989650+yigitkonur@users.noreply.github.com>
2023-12-10 02:24:25 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
     "fmt"
     "log"
-
-    "github.com/yourusername/yourproject/processor"
 )
 
 func main() {
-    config := processor.ProcessorConfig{
+    config := ProcessorConfig{
         JSONInputPath:  "/path/to/json/input",
         JSONOutputPath: "/path/to/json/output.json",
         CSVOutputPath:  "/path/to/csv/output.csv",
     }
 
-    fileProcessor := processor.NewFileProcessor(config)
+    fileProcessor := NewFileProcessor(config)
 
     if err := fileProcessor.StartProcessing(); err != nil {
         log.Fatalf("Error processing files: %v", err)
